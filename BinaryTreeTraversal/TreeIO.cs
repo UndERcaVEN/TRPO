62cfac55c28dcea71c0168d0c0bf7c03938ce0ac
91897 <9189759110z@gmail.com>
2023-10-08 17:11:38 +0300

    Added Solution

---
+++ BinaryTreeTraversal/TreeIO.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeTraversal
+{
+    internal class TreeIO
+    {
+        private readonly BinaryTree<int> _tree;
+        internal TreeIO()
+        {
+            _tree = new BinaryTree<int>();
+        }
+        internal void MainLoop()
+        {
+            while (true)
+            {
+                Console.Write("Press 1 to fill the tree, 2 to traverse it or q to quit");
+                char input = Console.ReadKey(true).KeyChar;
+                IOHelper.ClearCurrentConsoleLine();
+
+                switch (input)
+                {
+                    case '1':
+                        FillTree();
+                        break;
+                    case '2':
+                        PrintTree();
+                        break;
+                    case 'q':
+                        return;
+                }
+
+            }
+        }
+
+        private void FillTree()
+        {
+            bool inputParsed = int.TryParse(Console.ReadLine(), out int input);
+            
+            IOHelper.ClearCurrentConsoleLine();
+            if (!inputParsed)
+            {
+                return;
+            }
+            _tree.AddElement(input);
+        }
+
+        private void PrintTree()
+        {
+            Console.WriteLine("Select traversal method:\n1 - Pre-order\n2 - in-order\n3 - post-order");
+            string result;
+
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case '1':
+                    result = TreeTraversal.DepthFirstSearch(_tree); 
+                    break;
+                case '2':
+                    result = TreeTraversal.SymmetricalSearch(_tree);
+                    break;
+                case '3':
+                    result = TreeTraversal.PostOderSearch(_tree);
+                    break;
+                default:
+                    return;
+            }
+
+            Console.WriteLine($"\nThe current array is: {result}");
+        }
+    }
+}
---
+++ BinaryTreeTraversal/TreeTraversal.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace BinaryTreeTraversal
+{
+    public static class TreeTraversal
+    {
+        public static string DepthFirstSearch<T>(BinaryTree<T> tree)
+        {
+            var result = new StringBuilder();
+
+            AppendPreOrder(tree, 0, result);
+            return result.ToString();
+        }
+
+        public static string SymmetricalSearch<T>(BinaryTree<T> tree)
+        {
+            var result = new StringBuilder();
+
+            AppendSymmetrical(tree, 0, result);
+            return result.ToString();
+        }
+
+
+        public static string PostOderSearch<T>(BinaryTree<T> tree)
+        {
+            var result = new StringBuilder();
+
+            AppendPostOrder(tree, 0, result);
+            return result.ToString();
+        }
+
+        private static void AppendSymmetrical<T>(BinaryTree<T> tree, int index, StringBuilder result)
+        {
+            if (index >= tree.ElementsCount)
+            {
+                return;
+            }
+
+            int leftIndex = index * 2 + 1;
+            AppendSymmetrical(tree, leftIndex, result);
+
+            result.Append(" " + tree.ElementAt(index));
+
+            int rightIndex = index * 2 + 2;
+            AppendSymmetrical(tree, rightIndex, result);
+        }
+
+        private static void AppendPreOrder<T>(BinaryTree<T> tree, int index, StringBuilder result)
+        {
+            if (index >= tree.ElementsCount)
+            {
+                return;
+            }
+
+            result.Append(" " + tree.ElementAt(index).ToString());
+
+            int leftIndex = index * 2 + 1;
+            AppendPreOrder(tree, leftIndex, result);
+
+            int rightIndex = index * 2 + 2;
+            AppendPreOrder(tree, rightIndex, result);
+        }
+
+        private static void AppendPostOrder<T>(BinaryTree<T> tree, int index, StringBuilder result)
+        {
+            if (index >= tree.ElementsCount)
+            {
+                return;
+            }
+
+            int leftIndex = index * 2 + 1;
+            AppendPostOrder(tree, leftIndex, result);
+
+            int rightIndex = index * 2 + 2;
+            AppendPostOrder(tree, rightIndex, result);
+
+            result.Append(" " + tree.ElementAt(index).ToString());
+        }
+    }
+}
