3a74174fb1c4d9c8f66d4c8b2c4b1874a0e6a1d0
91897 <9189759110z@gmail.com>
2023-10-08 17:35:21 +0300

    Uploaded solution

---
+++ binary minheap/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace BinaryMinheap
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Swaps places of 2 array elements
+        /// </summary>
+        /// <typeparam name="T">array elements type</typeparam>
+        /// <param name="arr">array</param>
+        /// <param name="firstIndex">index of the first element</param>
+        /// <param name="secondIndex">index of the second element</param>
+        public static void Swap<T>(this T[] arr, int firstIndex, int secondIndex)
+        {
+            // Caller's responsibility to assert indexes are in range of the array
+            Debug.Assert(firstIndex >= 0 && secondIndex >= 0); 
+            Debug.Assert(firstIndex <= arr.Length && secondIndex <= arr.Length);
+
+            (arr[firstIndex], arr[secondIndex]) = (arr[secondIndex], arr[firstIndex]);
+        }
+    }
+}
