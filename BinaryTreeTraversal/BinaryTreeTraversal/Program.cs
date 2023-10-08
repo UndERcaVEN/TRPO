65b1c1e5716425403d80bcc1e5a3a9a75bcfc0a0
91897 <9189759110z@gmail.com>
2023-10-08 17:04:58 +0300

    First commit

---
+++ BinaryTreeTraversal/BinaryTreeTraversal/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinaryTreeTraversal;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        new TreeIO().MainLoop();
+    }
+
+    private static void MainLoop()
+    {
+        
+    }
+
+}
